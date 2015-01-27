eef018ab4209ea09f44556ec655c7250bce6563f
callumsong <ghksong@gmail.com>
2015-01-27 13:11:40 -0800

    increasing JS functionality - yet to test so will test with css

--- js/script.js
+++ js/script.js
@@ -1,67 +1,65 @@
-function Route(name, location, distance, incline) {
-  this.name        = name;
+function Route(location, distance, incline) {
   this.location    = location;
   this.distance    = distance;
   this.incline     = incline;
   this.routeRating = 0;
+}
+Route.prototype.numberMatch = function(a, b) { //Increases rating according to how close the value is to the user input
+  var differential = Math.abs(a - b);
+  if (differential <= 2) {
+    this.routeRating += 5;
+  } else if(differential <= 4) {
+    this.routeRating += 4;
+  } else if(differential <= 6) {
+    this.routeRating += 3;
+  } else if (differential <= 8) {
+    this.routeRating += 2;
+  } else {
+    this.routeRating += 1;
+  }
+}
+Route.prototype.locationRating = function(a, b) {
+  if (a == b){
+    this.routeRating += 1;
+  }
 }
 
 function Results() {
 this.groupRoutes = [];
 this.finalRatings = [];
 }
-
 Results.prototype.addRoute = function(route) {
   this.groupRoutes.push(route);
 };
-Results.prototype.addRating = function(route) {
-  this.finalRatings.push(route);
+Results.prototype.addRating = function(rating) {
+  this.finalRatings.push(rating);
 };
-
-var finalResults = new Results();
-
-finalResults.addRoute(new Route ("slu", "SLU", 2.5, 0));
-finalResults.addRoute(new Route ("belltown", "Belltown", 1.75, 0));
-
-
-function numberMatch(a, b) {
-  var distDiff = Math.abs(a - b);
-  if (distDiff <= 2) {
-    Route.prototype.routeRating += 5;
-  } else if(distDiff <= 4) {
-    Route.prototype.routeRating += 4;
-  } else if(distDiff <= 6) {
-    Route.prototype.routeRating += 3;
-  } else if (distDiff <= 8) {
-    Route.prototype.routeRating += 2;
-  } else {
-    Route.prototype.routeRating += 1;
+Results.prototype.getBestRoute = function(){ //Returns best route according to the ratings
+  for (var i = 0; i <= this.finalRatings.length; i++) {
+    if (this.groupRoutes[i].routeRating == this.finalRatings[0]) {
+      $("#location").append(this.groupRoutes[i].location);
+      $("#distance").append(this.groupRoutes[i].distance);
+      $("#incline").append(this.groupRoutes[i].incline);
+    }
   }
 }
+var results = new Results();
+
+results.addRoute(new Route ("SLU", 2.5, 0));
+results.addRoute(new Route ("Belltown", 1.75, 0));
 
 function compare() {
   var $location = $("#location").text(),
-      distance = Number($("#distance").text()),
-      incline  = Number($("#incline").text());
+       distance = Number($("#distance").text()),
+       incline  = Number($("#incline").text());
 
-  numberMatch(distance, groupRoutes[i].distance);
-  numberMatch(incline,  groupRoutes[i].incline);
-
-  if ($location == groupRoutes[i].location){
-    groupRoutes[i].routeRating += 1;
+  for (var i = 0; results.groupRoutes.length; i++) {
+    results.groupRoutes[i].numberMatch(distance, results.groupRoutes[i].distance);
+    results.groupRoutes[i].numberMatch(incline, results.groupRoutes[i].incline);
+    results.addRating(results.groupRoutes[i].routeRating);
   }
-
-
-  finalResults.addRating(groupRoutes[i]);// Push scores into an array finalRatings.
-  finalResults.sort(); //need to sort by number
-  for (var i = 0; i <= finalResults.finalRatings.length; i++) {
-    if (finalResults.groupRoutes[i] == finalResults.finalRatings[0]) {
-      return [finalResults.groupRoutes[i].location, finalResults.groupRoutes[i].distance, finalResults.groupRoutes[i].incline];
-    }
-  }
-  return groupRoutes[i].name;
-// If the first value of finalRatings is the rating of a Route, print the values and the map onto the html
-
+  results.sort(); //need to sort by number
+  results.getBestRoute(); // If the first value of finalRatings is the rating of a Route, print the values and the map onto the html
 }
 
 $("#findRoute").on("click", function(){
