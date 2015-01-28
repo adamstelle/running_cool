ce24a85dab328dfb4026090c469895d690545c2a
callumsong <ghksong@gmail.com>
2015-01-28 15:29:19 -0800

    login validation working

--- js/login.js
+++ js/login.js
@@ -8,13 +8,17 @@ $("#exit").on("click", function() {
   $(".popupContainer").toggle();
 });
 
-$("#log").on("click", function(){
-  Login();
-  $(".popupContainer").toggle();
-});
+$("#log").on("click", Login);
 
 function Login() {
+  if($(".valueError").length !== 0) {
+    $(".formError").remove();
+    $(".buttonstyle").after("<p class='formError'>You 'ave to insert your values properly man!</p>");
+    event.preventDefault();
+  } else {
   var $name = $("#loginName").val();
   console.log($name);
   $("#nameInsert").append("<h2>Hello: </h2><p>" + $name + "</p>");
+  $(".popupContainer").toggle();
+  }
 }
