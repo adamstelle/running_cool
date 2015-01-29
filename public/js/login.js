4f3cab165c9bb9c95e0250566ce959cecc159f55
Adam Stelle <adamstelle@gmail.com>
2015-01-29 14:05:53 -0800

    Merged stylings

--- public/js/login.js
+++ public/js/login.js
@@ -13,6 +13,10 @@ function Login() {
     $(".formError").remove();
     $("#loginName").before("<p class='formError'>You 'ave to insert your values properly mahn!</p>");
     event.preventDefault();
+  } else if ($("#loginName").val().length === 0 || $("#password").val().length === 0) {
+    $(".formError").remove();
+    $("#loginName").before("<p class='formError'>You 'ave to insert your values properly mahn!</p>");
+    event.preventDefault();
   } else {
   var $name = $("#loginName").val();
   $("#nameInsert").append("<h2 class='greetings'>Welcome <em class='red'>" + $name + "!</em></h2>");
